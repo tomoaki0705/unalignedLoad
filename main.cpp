119a7bd351f9833d77945bcef6ecbb01ce488005
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2019-12-12 17:31:47 +0900

    brush up the output message

--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@ char workArea[2048] = {0};// big enough memory area
 
 int main(int argc, char**argv)
 {
-	for( size_t i = 0;i < 64;i++ )
+	for( size_t i = 0;i < 16;i++ )
 	{
 		std::cout << "loading from " << i;
 		loadExperiment(workArea + i);
+		std::cout << " DONE" << std::endl;
 	}
 	return 0;
 }
